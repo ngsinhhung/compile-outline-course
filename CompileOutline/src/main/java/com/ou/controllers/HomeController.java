dbf045f1189c382821a326e3dd6aee14e473324f
Sinh Hùng <sinhhung.ng@gmail.com>
2024-06-03 16:36:28 +0700

    rebase

--- CompileOutline/src/main/java/com/ou/controllers/HomeController.java
+++ CompileOutline/src/main/java/com/ou/controllers/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/")
     public String index() {
-        return "admin";
+        return "index";
     }
 
     @RequestMapping("/admin/manage-outlines")
