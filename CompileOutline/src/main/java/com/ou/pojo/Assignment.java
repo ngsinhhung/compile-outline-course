e772f045bc28be4448156429f0320694d746f5fd
Sinh Hùng <sinhhung.ng@gmail.com>
2024-06-03 12:26:57 +0700

    add pojo assignments

---
+++ CompileOutline/src/main/java/com/ou/pojo/Assignment.java
@@ -0,0 +1,34 @@
+package com.ou.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "assignments")
+public class Assignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "Lecturer_User_id", nullable = false)
+    private Lecturer lecturerUser;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "Subject_id", nullable = false)
+    private Subject subject;
+
+    @Lob
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "assignment_date")
+    private Instant assignmentDate;
+
+}
